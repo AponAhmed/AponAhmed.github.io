ab3d6285bbfb80f055b4f38d844c3fd10b541450
MD. Muhiminul Haque <apon2041@gmail.com>
2025-04-16 18:10:57 +0600

    Add TeamPulse project details and images; update styles and webpack output path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js", // Adjust the entry point as needed
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public/assets"),
   },
   module: {
     rules: [
